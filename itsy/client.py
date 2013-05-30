068709d6dd93fffad57b18351902512db79a479b
Scott Torborg <scott@cartlogic.com>
2013-05-29 22:17:52 -0700

    Switch to googlebot as the default user agent

--- itsy/client.py
+++ itsy/client.py
@@ -2,9 +2,8 @@ import requests
 
 
 class Client(object):
-    default_user_agent = ('Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) '
-                          'AppleWebKit/536.26 (KHTML, like Gecko) '
-                          'Version/6.0 Mobile/10A5355d Safari/8536.25')
+    default_user_agent = ('Mozilla/5.0 (compatible; Googlebot/2.1; '
+                          '+http://www.google.com/bot.html)')
 
     def __init__(self, user_agent=default_user_agent, dnt=True, proxies=None):
         self.user_agent = user_agent
